2d91303162a853b31b95c9d98c2a6f6bbed1b217
abra-adl <sxawra1997@gmail.com>
2026-07-16 20:38:37 +0100

    Style signup form fields with Bootstrap classes and fix email not being saved on registration

--- assets/forms.py
+++ assets/forms.py
@@ -9,15 +9,25 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
-    
-    # def save(self, commit=True):
-    #     """
-    #     user.email = self.cleaned_data.get("email")
-    #     Overrides the base implementation to ensure the email field is saved.
-    #     """
-    #     user = super().save(commit=False)
-    #     user.email = self.cleaned_data["email"]
-    #     if commit:
-    #         user.save()
-    #         self._save_m2m()
-    #     return user
+
+    def __init__(self, *args, **kwargs):
+        """
+        Apply Bootstrap's form-control class to every field so inputs render
+        full-width and styled consistently with the rest of the site (login,
+        etc.), instead of falling back to tiny default browser input styling.
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control'})
+
+    def save(self, commit=True):
+        """
+        Overrides the base implementation to ensure the email field is
+        actually saved - the base UserCreationForm doesn't include email
+        by default, so without this override it would be silently dropped.
+        """
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
